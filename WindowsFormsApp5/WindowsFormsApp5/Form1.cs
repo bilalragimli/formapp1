9a2c199ec4c0706b85e3653b1402bb6fc471e4fe
bilalragimli <49587796+bilalragimli@users.noreply.github.com>
2019-07-22 23:55:39 +0400

    Add files via upload

---
+++ WindowsFormsApp5/WindowsFormsApp5/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        List<string> groupList;
+        public Form1()
+        {
+            groupList = new List<string>();
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            string groupname = txtbox.Text;
+            if (groupname.Length!=0)
+            {
+                groupList.Add(groupname);
+                cmb.Items.Clear();
+                foreach (var grp in groupList)
+                {
+                    cmb.Items.Add(grp);
+                }
+
+            }
+            else
+            {
+                error.Text = "Ənkə";
+                    error.Visible = true;
+            }
+        }
+    }
+}
